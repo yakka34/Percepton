34ab48903522e5b3911cf2c31c2b87a3fdad365e
yakka34 <tv.lehtinen@hotmail.com>
2026-07-01 18:19:15 +0300

    Clean test imports

--- src/test/java/com/github/percepton/function/VectorNegateFunctionTest.java
+++ src/test/java/com/github/percepton/function/VectorNegateFunctionTest.java
@@ -2,7 +2,7 @@ package com.github.percepton.function;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class VectorNegateFunctionTest {
 
